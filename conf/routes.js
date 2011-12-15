bd599cc753ec34c84c00be1017ce784ff26cdb79
Matt Brennan <mattyb1000@gmail.com>
2011-12-15 14:54:23 +0000

    in line with new struct api (ish)

--- conf/routes.js
+++ conf/routes.js
@@ -1,14 +1,16 @@
-exports.routes=function($)[
-	["*","/favicon.ico",this.staticFile,"static/favicon.ico"],
-	["*","/apple-touch-icon-114x114-precomposed.png",this.staticFile,"static/apple-touch-icon-114x114-precomposed.png"],
-	["*","/apple-touch-icon-72x72-precomposed.png",this.staticFile,"static/apple-touch-icon-72x72-precomposed.png"],
-	["*","/apple-touch-icon-57x57-precomposed.png",this.staticFile,"static/apple-touch-icon-57x57-precomposed.png"],
-	["*","/robots.txt",this.staticFile,"static/robots.txt"],
-	["*","/humans.txt",this.staticFile,"static/humans.txt"],
-	["*","/crossdomain.xml",this.staticFile,"static/crossdomain.xml"],
+const static = require("static.js"),
+$ = require("mvc/list.js").controllers;
+module.exports = [
+	["*","/favicon.ico",static.file,"static/favicon.ico"],
+	["*","/apple-touch-icon-114x114-precomposed.png",static.file,"static/apple-touch-icon-114x114-precomposed.png"],
+	["*","/apple-touch-icon-72x72-precomposed.png",static.file,"static/apple-touch-icon-72x72-precomposed.png"],
+	["*","/apple-touch-icon-57x57-precomposed.png",static.file,"static/apple-touch-icon-57x57-precomposed.png"],
+	["*","/robots.txt",static.file,"static/robots.txt"],
+	["*","/humans.txt",static.file,"static/humans.txt"],
+	["*","/crossdomain.xml",static.file,"static/crossdomain.xml"],
 	["GET","/", $.site.index],
 	["GET","/home", $.site.index],
 	["GET","/about", $.site.about],
 	["POST","/tasks/list",$.tasks.create],
-	["*","/{controller}/{action}",function(_) $[_.controller][_.action]]
+	["*","/{controller}/{action}",$("controller","action")]
 ]
